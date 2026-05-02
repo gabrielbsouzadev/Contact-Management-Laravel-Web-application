1992c6662f331504ef253d4b77276980536b5e0a
Gabriel Souza <60492389+gabrielbsouzadev@users.noreply.github.com>
2026-05-02 15:37:44 +0100

    Add name, contact, and email fields to contacts table

--- database/migrations/2026_05_02_142214_create_contacts_table.php
+++ database/migrations/2026_05_02_142214_create_contacts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('contact', 9);
+            $table->string('email');
             $table->timestamps();
         });
     }
